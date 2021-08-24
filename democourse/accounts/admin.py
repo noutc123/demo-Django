056993414910bf4f581b58101ee6103cc1bae223
noutcheu libert <libert.noutcheu@gmail.com>
2021-08-24 21:39:43 +0100

    reglage de git iginore

--- democourse/accounts/admin.py
+++ democourse/accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Custumer
 # Register your models here.
+
+admin.site.register(Custumer)
--- democourse/accounts/models.py
+++ democourse/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Custumer(models.Model):
+	"""
+	Description: Model Description
+	"""
+	name = models.CharField(max_length=200)
+	price = models.FloatField()
+	email =  models.CharField(max_length=200)
+	data_created = models.DateTimeField(auto_now_add=True, null=True)   
+
+	class Meta:
+		ordering = ('price',)
+	pass
